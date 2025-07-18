5743b78a472e3d832ca4371d286b9dcfaab85934
engai2025 <engai2025@gmail.com>
2025-07-18 16:07:20 +0100

    Added The Main stracture

---
+++ Main.js
@@ -0,0 +1,15 @@
+
+const API_URL = 'https://fakestoreapi.com/products/category/electronics';
+const productsGrid = document.getElementById('productsGrid');
+const cartBtn = document.getElementById('cartBtn');
+const cartModal = document.getElementById('cartModal');
+const closeCart = document.getElementById('closeCart');
+const cartItemsList = document.getElementById('cartItems');
+const cartTotal = document.getElementById('cartTotal');
+const checkoutBtn = document.getElementById('checkoutBtn');
+const cartCount = document.getElementById('cartCount');
+const searchInput = document.getElementById('searchInput');
+const messageModal = document.getElementById('messageModal');
+const closeMessage = document.getElementById('closeMessage');
+const messageContent = document.getElementById('messageContent');
+
